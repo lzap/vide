e5a89456d00fffa525f20c67610adb6e64b8ccea
Lukas Zapletal <lzap+git@redhat.com>
2011-12-20 10:25:06 +0100

    modifing release script - now works

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tito build --srpm || exit 1
-FROM="/data/temp/koji/vide-*"
+make dist
+FROM=vide*tar.gz
 TO=lzap@fedorapeople.org:/home/fedora/lzap/public_html/projects/vide/releases
 rsync -av -e ssh $FROM $TO
